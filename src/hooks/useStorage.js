5b5ea6d2fe1e5ea37973ed3772bcafc3f7b3fc48
yigitozdamar <yigitozdamar@hotmail.com>
2022-01-04 13:50:20 +0300

    firebase usage uploaded

--- src/hooks/useStorage.js
+++ src/hooks/useStorage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { projectStorage } from "../firebase/config";
 
 const useStorage = (file) => {
